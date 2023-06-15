640365e3af44c8935264fa0c1f6baff71e3712be
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-06-15 18:05:46 -0300

    feat: AuthService signIn method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async signIn(username: string, pass: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+
+    if (user?.password !== pass) {
+      throw new UnauthorizedException();
+    }
+
+    const { password, ...result } = user;
+    // TODO: generate jwt and returning it
+
+    return result;
+  }
+}
