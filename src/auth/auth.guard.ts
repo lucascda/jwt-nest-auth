ad3d1f44b36d2f7898e8b0c853ad5f9deda1b62f
Lucas César <43433070+lucascda@users.noreply.github.com>
2023-06-15 19:39:45 -0300

    AuthController getProfile method

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,18 +1,21 @@
 import {
   CanActivate,
   ExecutionContext,
+  Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { jwtConstants } from './constants';
 
+@Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
+
     if (!token) {
       throw new UnauthorizedException();
     }
@@ -21,9 +24,11 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret,
       });
+
       // assigning payload to request object, to access it later
       request['user'] = payload;
     } catch (error) {
+      console.log(error);
       throw new UnauthorizedException();
     }
     return true;
